9e7b0ad6f1d682f71d51adc57d7229f09578f7a7
bugax <joaopbugarin@gmail.com>
2025-11-23 22:58:00 +0000

    car fleet done.. just start by the last car, nice exercise

--- 853.py
+++ 853.py
@@ -1,2 +1,22 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = []
+        fleets = []
+        pos = [target-position[n] for n in range(len(position))]
+        print(sol)
+        for i in range(len(pos)):
+            time = pos[i]/speed[i]
+            cars.append((pos[i],time))
+        cars.sort(key=lambda x: x[0])
+        print('cars:',cars)
+        for car in cars:
+            print("car:",car,"cars:",cars)
+            if fleets and car[1]<=fleets[-1]:
+                continue
+            else:
+                fleets.append(car[1])
+        print(fleets)
+        return len(fleets)
+
+sol = Solution()
+print(sol.carFleet(12,[10,8,0,5,3],[2,4,1,1,3]))
