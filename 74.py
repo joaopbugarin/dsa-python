4cb46f882b16cd8cec4b1959863cc75fe5925379
bugax <joaopbugarin@gmail.com>
2025-11-24 23:49:31 +0000

    search a 2d matrix done

--- 74.py
+++ 74.py
@@ -1,12 +1,22 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row_len = (len(matrix[0]))
-        matrix_len = len(matrix)
-        print(row_len, matrix_len)
+        cols = (len(matrix[0]))
+        rows = len(matrix)
+        #the middle element of index m will sit at
+        # m = matrix[m // number of columns][m % number of columns]
         l = 0
-        r = row_len - 1 + row_len * (matrix_len - 1)
-        m = (l + r) // 2
-        print(l,m,r)
+        r = rows * cols -1
+        print(l,r)
+        while (l <= r):
+            m = (l + r) // 2
+            curr_m = matrix[m // cols][m % cols]
+            if curr_m == target:
+                return True
+            if target > curr_m:
+                l = m + 1
+            elif target < curr_m:
+                r = m -1
+        return False
 
 sol = Solution()
 print(sol.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))
