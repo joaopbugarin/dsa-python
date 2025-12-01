7f1c5a80063c380a891afaea5a7cc17b9d307a4d
bugax <joaopbugarin@gmail.com>
2025-12-01 22:53:58 +0000

    permutation in string refactor

--- 567.py
+++ 567.py
@@ -1,19 +1,24 @@
 from collections import defaultdict
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        left, count_s1, count_s2 = 0, defaultdict(int), defaultdict(int)
+        count_s1 = defaultdict(int)
+        count_s2 = defaultdict(int)
+
         for char in s1:
             count_s1[char] += 1
-        for right in range(len(s2)):
-            count_s2[s2[right]] += 1
-            if count_s2[s2[right]] > count_s1[s2[right]] or s2[right] not in count_s1:
-                count_s2.clear()
-                left = right + 1
-            print("s1_count:",count_s1)
-            print("s2_count:",count_s2)
-            if right - left + 1 == len(s1):
-                return True
-            return False
 
+        l = 0
+        for r in range(len(s2)):
+            count_s2[s2[r]] += 1
+
+            while count_s2[s2[r]] > count_s1[s2[r]]:
+                count_s2[s2[l]] -= 1
+                l += 1
+
+            if r - l + 1 == len(s1):
+                return True
+
+        return False
 sol = Solution()
 print(sol.checkInclusion("ab","edibaooo"))
