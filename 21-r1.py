4df3e9ee0c9691f283ce063c47857b25a9136dfe
Bugarin <joaopbugarin@gmail.com>
2026-02-10 23:56:47 +0000

    redid merge lists, easy exc but took me a while

--- 21-r1.py
+++ 21-r1.py
@@ -5,3 +5,19 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        new_list = ListNode()
+        curr = new_list
+        while list1 or list2:
+            val1 = list1.val if list1 else float('inf')
+            val2 = list2.val if list2 else float('inf')
+            if val1 <= val2:
+                curr.next = ListNode(val1)
+                list1 = list1.next if list1 else None
+            else:
+                curr.next = ListNode(val2)
+                list2 = list2.next if list2 else None
+
+            curr = curr.next
+
+        return new_list.next
+
